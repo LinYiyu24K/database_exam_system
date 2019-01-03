6733eb4a634eee114877bc458719873be36d90e1
Evecang <547624569@qq.com>
2019-01-04 01:43:40 +0800

    teacher和manager的登陆逻辑修复

--- src/server/sqlMap.js
+++ src/server/sqlMap.js
@@ -6,8 +6,8 @@ var sqlMap = {
         addTeacher: 'insert into teacher(tno, password,tname, tsex,usertype,tphone,classname) values (?,?,?,?,?,?,?)',
         addManager: 'insert into manager(dno, password,dname, dsex,usertype,dphone) values (?,?,?,?,?,?)',
         findStudent: 'select * from student where sno=? and password=?',
-        findTeacher: 'select * from teacher where sno=? and password=?',
-        findManager: 'select * from manager where sno=? and password=?'
+        findTeacher: 'select * from teacher where tno=? and password=?',
+        findManager: 'select * from manager where dno=? and password=?'
     }
 }
 
